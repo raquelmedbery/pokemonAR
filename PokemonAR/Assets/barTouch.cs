498423312c6c37b38e7b8f6ccb8ca76d8ccbfb39
raquelmedbery <50762368+raquelmedbery@users.noreply.github.com>
2019-10-14 19:03:09 -0500

    adding project
    fixed .gitignore (had to take it out of folder and save without extension) and also added Unity project

---
+++ PokemonAR/Assets/barTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barTouch : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("none") && gameObject.activeSelf)
+            {
+                anim.Play("bars");
+                //gameObject.SetActive(true);
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("bars") && gameObject.activeSelf)
+            {
+                anim.Play("none");
+            }
+        }
+
+    }
+}
---
+++ PokemonAR/Assets/pokeTouch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pokeTouch : MonoBehaviour
+{
+
+    public GameObject lightning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lightning.SetActive(false);
+        lightning = GameObject.Find("poly_lightning");
+        lightning.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!(lightning.activeSelf))
+        {
+            print("Lightning should NOT be appearing");
+        }
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+
+            if (Input.GetTouch(i).phase == TouchPhase.Began && !(lightning.activeSelf))
+            {
+
+                lightning.SetActive(true);
+            }
+
+        }
+
+        /*for (int i = 0; i < Input.touchCount; ++i)
+        { 
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Instantiate(lightning, touchPos, Quaternion.identity);
+            }
+        }*/
+
+    }
+}
